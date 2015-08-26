908beb1f07783eab3fd60ef57d64b9310d932155
qianye <422794901@qq.com>
2015-08-26 13:09:22 +0800

    Initial Commit

---
+++ QYRippleAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QYRippleAnimation
+//
+//  Created by qianye on 15/8/26.
+//  Copyright (c) 2015年 qianye. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
